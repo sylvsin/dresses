0518e56b9db947c04837440cbc6dfe5600116a9e
sylvain lubwimi <lubwimi@gmail.com>
2021-01-27 22:44:57 +0100

    create orders

--- src/components/Administration/Admin.tsx
+++ src/components/Administration/Admin.tsx
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './Admin.css';
+import Orders from '../Oders/Oders';
+import { DressContext } from '../../context/DressContext';
 
 const Admin: React.FC = () => {
+  const { orders } = useContext(DressContext);
+
   return (
-    <div className="admin">
-      <span>Admin works!</span>
+    <div>
+      <h2>Orders</h2>
+      <div className="admin">
+                <div className="order-row orders-header"> 
+          <span className="center">ID</span>
+          <span className="center">DATE</span>
+          <span className="center">TOTAL</span>
+          <span className="center">NAME</span>
+          <span className="center">EMAIL</span>
+          <span className="center">ADDRESS</span>
+          <span className="center">ITEMS</span>
+        </div>
+
+        {
+          orders.map((order, i) => {
+            return (
+              <Orders order={order} key={`${order._id}-${order.name}-${i}`} />
+            )
+          })
+        }
+      </div>
     </div>
   );
 }
